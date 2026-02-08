9c04706242b930a6345c2d39a72bfca653a373ea
Paul Andrei Vicente <paulandreivicente@gmail.com>
2026-02-08 10:54:59 +0800

    your commit message

--- src/components/game/chapters/index.ts
+++ src/components/game/chapters/index.ts
@@ -2,4 +2,5 @@
 export { CHAPTER_1 } from './chapter1';
 export { CHAPTER_2 } from './chapter2';
 export { CHAPTER_3 } from './chapter3';
-export { CHAPTER_4, CHAPTER_5, CHAPTER_6 } from './chapters456';
+export { CHAPTER_4 } from './chapter4';
+export { CHAPTER_5, CHAPTER_6 } from './chapters56';
